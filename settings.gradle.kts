00b81a46923af1e77f47d8754406c6fe399eb29f
be:theproud <betheproud@gmail.com>
2025-05-21 14:30:20 +0900

    [feat] task-master init

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,14 @@
 rootProject.name = "hello-spring-boot-3-kotlin-by-vibe-coding"
+
+include(
+    "app:app-api",
+    "app:app-batch",
+    "core",
+    "domain:rds",
+    "framework:client:base",
+    "framework:client:build",
+    "framework:client:kakao",
+    "framework:provider:auth",
+    "framework:provider:jwt",
+    "framework:redis"
+)
